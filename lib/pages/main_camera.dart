e1ba9812707a075323ffb68a4a446cd51655c5ed
angganyobaIT <anggaradennise7@gmail.com>
2026-05-22 19:55:20 +0700

    camera feature

--- lib/pages/main_camera.dart
+++ lib/pages/main_camera.dart
@@ -1,18 +1,30 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
+import 'package:slicing_pbm/main.dart' show cameras;
+import 'package:slicing_pbm/pages/preview_camera.dart';
 
 class MainCamera extends StatefulWidget {
   final List<CameraDescription> cameras;
   const MainCamera({Key? key, required this.cameras}) : super(key:key);
 
   @override
-  State<MainCamera> createState() => _MyWidgetState();
+  State<MainCamera> createState() => _MainCameraState();
 }
 
-class _MyWidgetState extends State<MainCamera> {
+class _MainCameraState extends State<MainCamera> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      child: IconButton(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => PreviewCamera(cameras: cameras),
+          ),
+        ),
+        icon: Icon(Icons.camera_alt),
+      ), // IconButton
+    ); // Container
   }
 }
--- lib/pages/preview_camera.dart
+++ lib/pages/preview_camera.dart
@@ -2,6 +2,8 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
+import '../main.dart';
+
 
 class PreviewCamera extends StatefulWidget {
   final List<CameraDescription>cameras;
@@ -18,18 +20,48 @@ class _PreviewCameraState extends State<PreviewCamera> {
   int kameraIndex =0;
 
   @override
-  void InitState(){
+  void initState(){
     super.initState();
-    _initCamera(cameraController);
+    _initCamera(cameras[kameraIndex]);
+  }
+  @override
+  void dispose(){
+    _cameraController.dispose();
+    super.dispose();
   }
   
+  
   void _initCamera(CameraDescription cameraController){
-    _cameraController=CameraController(cameraController, ResolutionPreset.high);
-    _initializeCameraController=_cameraController.initialize();
+    _cameraController = CameraController(cameraController, ResolutionPreset.high);
+    _initializeCameraController = _cameraController.initialize();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+ @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("Kamera"),
+    ),
+    body: FutureBuilder<void>(
+      future: _initializeCameraController,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text("Terjadi error"),
+          );
+        }
+
+        return Stack(
+          children: [
+            CameraPreview(_cameraController),
+          ],
+        );
+      },
+    ),
+  );
+}
 }
